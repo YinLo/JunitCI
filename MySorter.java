df82461f5189977e4782b1a44e0e3dfbb572ed84
ycLo <wdo564211@gmail.com>
2016-12-25 18:52:06 +0800

    success Testing

--- MySorter.java
+++ MySorter.java
@@ -1,5 +1,3 @@
-package ntou.cs.sorter;
-
 public class MySorter
 {
 	public void sort (double[] d)
@@ -8,7 +6,7 @@ public class MySorter
 		{
 			for (int j = 0; j < d.length - i - 1; j++)
 			{
-				if (d[j] > d[j + 1])
+				if (Math.abs(d[j]) > Math.abs(d[j + 1]))
 				{
 					swap(d, j, j + 1);
 				}
